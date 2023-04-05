3ef28f2c6d18a2029a7d74d003709b9c4ec15639
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-05 21:39:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+document.querySelector("button").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*50+1);
+    document.querySelector(".field1").textContent=numQuiniela;
+    document.querySelector(".field1").style.fontSize="2rem";
+});
+
+document.querySelector("button").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*50+1);
+    document.querySelector(".field2").textContent=numQuiniela;
+    document.querySelector(".field2").style.fontSize="2rem";
+});
+
+document.querySelector("button").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*50+1);
+    document.querySelector(".field3").textContent=numQuiniela;
+    document.querySelector(".field3").style.fontSize="2rem";
+});
+
+document.querySelector("button").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*50+1);
+    document.querySelector(".field4").textContent=numQuiniela;
+    document.querySelector(".field4").style.fontSize="2rem";
+});
+
+document.querySelector("button").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*50+1);
+    document.querySelector(".field5").textContent=numQuiniela;
+    document.querySelector(".field5").style.fontSize="2rem";
+});
+
+

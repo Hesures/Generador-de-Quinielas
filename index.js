be68895b2b74faa783e78a1c8cf470100ce397b7
Hesures <128929471+Hesures@users.noreply.github.com>
2023-04-05 22:12:55 +0200

    Primitiva añadida

--- index.js
+++ index.js
@@ -1,33 +1,81 @@
 
-
-document.querySelector("button").addEventListener("click", function () {
+// QUINIELA
+document.querySelector(".btn1").addEventListener("click", function () {
     var numQuiniela = Math.floor(Math.random()*50+1);
     document.querySelector(".field1").textContent=numQuiniela;
     document.querySelector(".field1").style.fontSize="2rem";
 });
 
-document.querySelector("button").addEventListener("click", function () {
+document.querySelector(".btn1").addEventListener("click", function () {
     var numQuiniela = Math.floor(Math.random()*50+1);
     document.querySelector(".field2").textContent=numQuiniela;
     document.querySelector(".field2").style.fontSize="2rem";
 });
 
-document.querySelector("button").addEventListener("click", function () {
+document.querySelector(".btn1").addEventListener("click", function () {
     var numQuiniela = Math.floor(Math.random()*50+1);
     document.querySelector(".field3").textContent=numQuiniela;
     document.querySelector(".field3").style.fontSize="2rem";
 });
 
-document.querySelector("button").addEventListener("click", function () {
+document.querySelector(".btn1").addEventListener("click", function () {
     var numQuiniela = Math.floor(Math.random()*50+1);
     document.querySelector(".field4").textContent=numQuiniela;
     document.querySelector(".field4").style.fontSize="2rem";
 });
 
-document.querySelector("button").addEventListener("click", function () {
+document.querySelector(".btn1").addEventListener("click", function () {
     var numQuiniela = Math.floor(Math.random()*50+1);
     document.querySelector(".field5").textContent=numQuiniela;
     document.querySelector(".field5").style.fontSize="2rem";
 });
 
+document.querySelector(".btn1").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*12+1);
+    document.querySelector(".field6").textContent=numQuiniela;
+    document.querySelector(".field6").style.fontSize="2rem";
+});
 
+document.querySelector(".btn1").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*12+1);
+    document.querySelector(".field7").textContent=numQuiniela;
+    document.querySelector(".field7").style.fontSize="2rem";
+});
+
+// PRIMITIVA
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field8").textContent=numQuiniela;
+    document.querySelector(".field8").style.fontSize="2rem";
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field9").textContent=numQuiniela;
+    document.querySelector(".field9").style.fontSize="2rem";
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field10").textContent=numQuiniela;
+    document.querySelector(".field10").style.fontSize="2rem";
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field11").textContent=numQuiniela;
+    document.querySelector(".field11").style.fontSize="2rem";
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field12").textContent=numQuiniela;
+    document.querySelector(".field12").style.fontSize="2rem";
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    var numQuiniela = Math.floor(Math.random()*49+1);
+    document.querySelector(".field13").textContent=numQuiniela;
+    document.querySelector(".field13").style.fontSize="2rem";
+});
